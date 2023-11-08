1a9bf7398bcc839ba801ce707c217e3ec49907dd
guntekhunter <guntek2000@gmail.com>
2023-11-08 18:45:44 +0800

    fix: promo filter

--- src/pages/Promo.jsx
+++ src/pages/Promo.jsx
@@ -3,7 +3,7 @@ import Title from "../component/Title";
 import ContentProfider from "../component/ContentProfider";
 import PromoImage from "../assets/promo.jpg";
 import Main from "../component/Main";
-import Card from "../component/Card";
+import Button from "../component/Button";
 import "../css/promo.css";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -28,16 +28,47 @@ export default function Promo() {
     navigate("/adress");
   };
 
-  console.log(promoData);
+  const handleFilter = async (type) => {
+    try {
+      const response = await axios.get("http://localhost:3333/promo");
+      const responseData = response.data;
+
+      if (type === "all") {
+        setPromoData(response.data);
+      } else {
+        const filteredData = responseData.filter((item) => item.type === type);
+        setPromoData(filteredData);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       <ContentProfider>
         <Title title="Promo" />
         <Main>
           <div className="flex space-x">
-            <Card className="filter">Ommaleka</Card>
-            <Card className="filter">Ommaleka</Card>
-            <Card className="filter">Ommaleka</Card>
+            <Button
+              className="filter"
+              title="Platinum"
+              onClick={() => handleFilter("platinum")}
+            />
+            <Button
+              className="filter"
+              title="Silver"
+              onClick={() => handleFilter("silver")}
+            />
+            <Button
+              className="filter"
+              title="Golden"
+              onClick={() => handleFilter("golden")}
+            />
+            <Button
+              className="filter"
+              title="All"
+              onClick={() => handleFilter("all")}
+            />
           </div>
           <div className="between-down mt-1">
             {/* card */}
