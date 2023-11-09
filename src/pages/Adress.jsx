be3bfd89c1f7c2fb504ea25120c6739e8ced24eb
guntekhunter <guntek2000@gmail.com>
2023-11-09 09:27:59 +0800

    feat: maps

--- src/pages/Adress.jsx
+++ src/pages/Adress.jsx
@@ -9,6 +9,8 @@ import Button from "../component/Button";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
+import { Loader } from "@googlemaps/js-api-loader";
+
 export default function Adress() {
   const [adress, setAdress] = useState();
   const [destination, setDestination] = useState();
@@ -18,6 +20,9 @@ export default function Adress() {
     localStorage.setItem("destination", destination);
     navigate("/order");
   };
+
+  console.log(Loader);
+
   return (
     <div>
       <ContentProfider>
@@ -42,6 +47,17 @@ export default function Adress() {
               <Button title="Send" onClick={selectFleet} />
             </div>
           </Card>
+          <div className="map">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3973.9343854876556!2d119.42599948404211!3d-5.11427769306157!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbefd26b0918735%3A0xa010d7db0ba8cbe0!2sKelompok%20Bermain%20Anggrek!5e0!3m2!1sid!2sid!4v1699492893334!5m2!1sid!2sid"
+              width="400"
+              height="300"
+              style={{ border: "0" }}
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
         </Main>
       </ContentProfider>
     </div>
